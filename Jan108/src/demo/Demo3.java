847c2c0206f2e10c6ffb67adf1d379858ac91fea
Messiless <wangzc946971@gamil.com>
2017-01-15 19:16:50 +0800

    update
    update

---
+++ Jan108/src/demo/Demo3.java
@@ -0,0 +1,10 @@
+package demo;
+
+/**
+ * Created by Computer on 2017/1/15.
+ */
+public class Demo3 {
+    public static void main(String[] args) {
+        System.out.println("just a try");
+    }
+}
