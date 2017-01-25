16afb30c71748174294950ec369866aa0e37f482
Messiless <wangzc946971@gmail.com>
2017-01-25 23:58:28 +0800

    add
    add

---
+++ day20/src/web/FirstFilter.java
@@ -0,0 +1,24 @@
+package web;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by Computer on 2017/1/25.
+ */
+public class FirstFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ day20/src/web/TargetServlet.java
@@ -0,0 +1,17 @@
+package web;
+
+import java.io.IOException;
+
+/**
+ * Created by Computer on 2017/1/25.
+ */
+@javax.servlet.annotation.WebServlet(name = "TargetServlet")
+public class TargetServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+doGet(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("shuaibi");
+    }
+}
