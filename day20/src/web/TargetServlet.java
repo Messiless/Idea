b1c75c89a342b246bcd7ecd28515147be5aaf70a
Messi <wangzc946971@gmail.com>
2017-01-28 11:40:30 +0800

    Update TargetServlet.java

--- day20/src/web/TargetServlet.java
+++ day20/src/web/TargetServlet.java
@@ -10,7 +10,7 @@ public class TargetServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 doGet(request,response);
     }
-
+//just a contribute
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println("shuaibi");
     }
