2461d8ae9ff688423384a301b48060877fa442b1
Messiless <wangzc946971@gmail.com>
2017-02-01 23:23:31 +0800

    add
    add

--- day20/src/dao/MyHttp.java
+++ day20/src/dao/MyHttp.java
@@ -1,9 +1,15 @@
 package dao;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Computer on 2017/1/27.
  */
-public class MyHttp extends HttpServletRequest {
+public class MyHttp extends HttpServletResponse {
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        return null;
+    }
 }
